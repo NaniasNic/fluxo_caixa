3a83ab6b43f6f78bf453824bc68e5f92e75a2175
NaniasNic <ananiasdias0@gmail.com>
2022-07-14 21:29:15 -0300

    fluxo caixa

---
+++ Fluxo_Caixa.sql
@@ -0,0 +1,7 @@
+insert into cad_usuario(nome, email, senha, telefone, CPF, Cartão_Social, data_nascimento)
+values ("Cristina Vieira da Silva","critinavs123@gmail.com", "123456789", "8332927581","50813171520", "12345", "19970520");
+
+insert into cad_usuario(nome, email, senha, telefone, CPF, Cartão_Social, data_nascimento)
+values("Maria Eduarda Ribeiro", "dudaribeiro@gmail.com", "50406034", "2431626628", "10068780923", "20107", "20001012");
+
+select * from cad_usuario
